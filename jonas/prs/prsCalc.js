60b3d5cb846a2be6ca804aa7f3b6b850e3d0402b
Jonas Almeida <jalmeida@mathbiol.org>
2022-11-30 07:55:49 -0500

    activating require

--- jonas/prs/prsCalc.js
+++ jonas/prs/prsCalc.js
@@ -1,4 +1,7 @@
 (async()=>{
-    // prsCalc = await import('https://episphere.github.io/app/jonas/prs/export.js')
-    prsCalc = await import('http://localhost:8000/app/jonas/prs/export.js')
+    prsCalc = await import('https://episphere.github.io/app/jonas/prs/export.js')
+    // prsCalc = await import('http://localhost:8000/app/jonas/prs/export.js')
+    if(typeof(define)!='undefined'){
+        define(prsCalc)
+    }
 })()
