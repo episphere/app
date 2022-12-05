252aa80e18801786aaea099258687a43a628ec35
Jonas Almeida <jalmeida@mathbiol.org>
2022-12-04 21:22:30 -0500

    mutation match

--- jonas/prs/export.js
+++ jonas/prs/export.js
@@ -9,6 +9,14 @@ if(typeof(pgs)=='undefined'){
     s.src='https://episphere.github.io/pgs/pgs.js'
     document.head.appendChild(s)
 }
+if(typeof(JSZip)=='undefined'){
+    let s = document.createElement('script')
+    s.src='https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js'
+    document.head.appendChild(s)
+}
+
+
+
 // pgs is now in the global scope, if it was not there already
 // import * as zip from "https://deno.land/x/zipjs/index.js"
 
@@ -21,7 +29,7 @@ let PGS23 = { // a global variable that is not shared by export
 PGS23.loadPGS = async (i=4)=>{ // startng with a default pgs
     let div = PGS23.divPGS
     div.innerHTML=`<b style="color:maroon">A)</b> PGS # <input id="pgsID" value=${i} size=5> <button id='btLoadPgs'>load</button>
-    <span id="summarySpan" hidden=true>[<a id="urlPGS" href='' target="_blank">source</a>]<br><span id="trait_mapped">...</span>, <span id="dataRows">...</span> variants, [<a id="pubDOI" target="_blank">Pub</a>], [<a href="#" id="objJSON">JSON</a>].</span>
+    <span id="summarySpan" hidden=true>[<a id="urlPGS" href='' target="_blank">source</a>]<span id="largeFile"><input id="checkLargeFile"type="checkbox">large file</span><br><span id="trait_mapped">...</span>, <span id="dataRows">...</span> variants, [<a id="pubDOI" target="_blank">Pub</a>], [<a href="#" id="objJSON">JSON</a>].</span>
     <p><textarea id="pgsTextArea" style="background-color:black;color:lime" cols=60 rows=5>...</textarea></p>`;
     div.querySelector('#pgsID').onkeyup=(evt=>{
         if(evt.keyCode==13){
@@ -30,26 +38,40 @@ PGS23.loadPGS = async (i=4)=>{ // startng with a default pgs
     })
     PGS23.pgsTextArea = div.querySelector('#pgsTextArea')
     div.querySelector('#btLoadPgs').onclick=async (evt)=>{
-        PGS23.pgsTextArea.value='loading ...'
+        PGS23.pgsTextArea.value='... loading'
         i = parseInt(div.querySelector('#pgsID').value)
         let PGSstr = i.toString()
         PGSstr = "PGS000000".slice(0,-PGSstr.length)+PGSstr
         div.querySelector('#urlPGS').href=`https://ftp.ebi.ac.uk/pub/databases/spot/pgs/scores/${PGSstr}/ScoringFiles/Harmonized/`
-        PGS23.pgsObj = await parsePGS(i)
         div.querySelector('#summarySpan').hidden=false
-        div.querySelector('#pubDOI').href='https://doi.org/'+PGS23.pgsObj.meta.citation.match(/doi\:.*$/)[0]
-        div.querySelector('#trait_mapped').innerHTML=`<span style="color:maroon">${PGS23.pgsObj.meta.trait_mapped}</span>`
-        div.querySelector('#dataRows').innerHTML=PGS23.pgsObj.dt.length
-        if(PGS23.pgsObj.txt.length<100000){
-            PGS23.pgsTextArea.value = PGS23.pgsObj.txt
-        }else{
-            PGS23.pgsTextArea.value = PGS23.pgsObj.txt.slice(0,100000)+`...\n... (${PGS23.pgsObj.dt.length} variants) ...`
-        }
-        //PGS23.data.pgs=pgsObj
-		let cleanObj = structuredClone(PGS23.pgsObj)
-		cleanObj.info=cleanObj.txt.match(/^[^\n]*/)[0]
-		delete cleanObj.txt
-		PGS23.data.pgs=cleanObj
+		//check pgs file size
+		let fsize = (await fetch(`https://ftp.ebi.ac.uk/pub/databases/spot/pgs/scores/${PGSstr}/ScoringFiles/Harmonized/${PGSstr}_hmPOS_GRCh37.txt.gz`,{method:'HEAD'})).headers.get('Content-Length');
+		if(fsize>10000000){
+			PGS23.pgsTextArea.value+=` ... whoa! ... this is a large PGS entry, over ${Math.floor(fsize/1000000)}Mb, \n\nIf you still want to process it please check "large file" above and press load again.`
+			div.querySelector('#largeFile').style.backgroundColor='yellow'
+			div.querySelector('#largeFile').style.color='red'
+			setTimeout(_=>{
+				div.querySelector('#largeFile').style.backgroundColor=''
+				div.querySelector('#largeFile').style.color=''
+			},2000)
+			//debugger
+		}else{
+			PGS23.pgsObj = await parsePGS(i)
+	        div.querySelector('#pubDOI').href='https://doi.org/'+PGS23.pgsObj.meta.citation.match(/doi\:.*$/)[0]
+	        div.querySelector('#trait_mapped').innerHTML=`<span style="color:maroon">${PGS23.pgsObj.meta.trait_mapped}</span>`
+	        div.querySelector('#dataRows').innerHTML=PGS23.pgsObj.dt.length
+	        if(PGS23.pgsObj.txt.length<100000){
+	            PGS23.pgsTextArea.value = PGS23.pgsObj.txt
+	        }else{
+	            PGS23.pgsTextArea.value = PGS23.pgsObj.txt.slice(0,100000)+`...\n... (${PGS23.pgsObj.dt.length} variants) ...`
+	        }
+	        //PGS23.data.pgs=pgsObj
+			const cleanObj = structuredClone(PGS23.pgsObj)
+			cleanObj.info=cleanObj.txt.match(/^[^\n]*/)[0]
+			delete cleanObj.txt
+			PGS23.data.pgs=cleanObj
+		}
+			
         //debugger
     };
     div.querySelector("#objJSON").onclick=evt=>{
@@ -78,6 +100,7 @@ PGS23.load23 = async ()=>{
 			}
 			PGS23.data.my23=my23
 		}
+		div.querySelector("#my23TextArea").value='... loading'
 		let readTxt = new FileReader()
 		let readZip = new FileReader()
 		readTxt.onload=ev=>{
@@ -86,10 +109,24 @@ PGS23.load23 = async ()=>{
 			//let my23 = parse23(txt,evt.target.files[0].name)
 			UI23(parse23(txt,evt.target.files[0].name))
 		}
-		readZip.readAsArrayBuffer=async ev=>{
-			await ev.arrayBuffer(x=>{
-				debugger
+		// readZip.readAsArrayBuffer=async ev=>{
+		readZip.onload=ev=>{
+			let zip = new JSZip()
+			zip.loadAsync(ev.target.result).then(zip=>{
+				//txtFname=Object.keys(zip.files)[0]
+				//console.log(zip.files,Date())
+				//console.log(Object.getOwnPropertyNames(zip.files)[0])
+				let fnametxt=Object.getOwnPropertyNames(zip.files)[0]
+				zip.file(fnametxt).async('string').then(txt=>{
+					div.querySelector("#my23TextArea").value=txt.slice(0,10000).replace(/[^\r\n]+$/,'')+'\n\n .................. \n\n'+txt.slice(-300).replace(/^[^\r\n]+/,'')
+					UI23(parse23(txt,evt.target.files[0].name))
+				})
+				//debugger
 			})
+			//debugger
+			//await ev.arrayBuffer(x=>{
+			//	debugger
+			//})
 			//let txt=await pako.inflate(ev.arrayBuffer(), { to: 'string' })
 			//debugger
 		}
@@ -108,19 +145,60 @@ PGS23.load23 = async ()=>{
 PGS23.loadCalc = async ()=>{
 	let div=PGS23.divCalc
 	div.innerHTML=`<hr>
-	<b style="color:maroon">C)</b> Relative risk calculation, the easiest bit,
-	next week :-),
-	<p><button id="buttonCalculateRisk">Calculate Relative Risk</button><span id="IsaidNextWeek" style="color:red;background-color:yellow" hidden=true> I said next week ;-)</span></p>
+	<b style="color:maroon">C)</b> Risk calculation.
+	<p><button id="buttonCalculateRisk">Calculate Risk</button><span id="hidenCalc" hidden=true>
+	[matches][calculation]</span>
+    </p>
+	<textarea id="my23CalcTextArea" style="background-color:black;color:lime" cols=60 rows=5>...</textarea>
 	<p>If you want to see the current state of the two data objects try <code>data = document.getElementById("PGS23calc").PGS23data</code> in the browser console</p>`
 	div.querySelector('#buttonCalculateRisk').onclick=evt=>{
-		let nextWeek=div.querySelector('#IsaidNextWeek')
-		nextWeek.hidden=false
-		div.querySelector('#buttonCalculateRisk')
-		setTimeout(function(){
-			nextWeek.hidden=true
-		},1000)
+		let hidenCalc=div.querySelector('#hidenCalc')
+		let my23TextArea = div.querySelector('#my23CalcTextArea')
+		my23CalcTextArea.value = '...'
+		hidenCalc.hidden=true
+		document.querySelector('#buttonCalculateRisk').disabled=true
+		document.querySelector('#buttonCalculateRisk').style.color='silver'
+		//div.querySelector('#buttonCalculateRisk')
+		let data = document.getElementById("PGS23calc").PGS23data
+		if(!data.pgs){
+			my23CalcTextArea.value+='\n... no PGS entry selected, please do that in A.'
+		}
+		if(!data.my23){
+			my23CalcTextArea.value+='\n... no 23andme report provided, please do that in B.'
+		}
+		if((!!data.my23)&(!!data.pgs)){
+			my23CalcTextArea.value=` ... looking for matches amongst the ${data.my23.dt.length} mutations targeted by 23andMe, for the reference ${data.pgs.dt.length} mutations reported in PGS#${data.pgs.id}, putatively associated with ${data.pgs.meta.trait_mapped} ...`
+			setTimeout(_=>{
+				PGS23.Calc(data)
+				my23CalcTextArea.value+=`\n ... ${data.pgsMatchMy23.length} found!`
+				hidenCalc.hidden=false
+				document.querySelector('#buttonCalculateRisk').disabled=false
+				document.querySelector('#buttonCalculateRisk').style.color='blue'
+			},100)
+		}
+		
+		
 		//debugger
 	}
+}
+
+PGS23.Calc = function (data){
+	// extract harmonized data from PGS entry first
+	const indChr = data.pgs.cols.indexOf('hm_chr')
+	const indPos = data.pgs.cols.indexOf('hm_pos')
+	// match
+	let dtMatch=[]
+	const cgrInd = data.pgs.cols.indexOf('hm_chr')
+	const posInd = data.pgs.cols.indexOf('hm_pos')
+	data.pgs.dt.forEach((r,i)=>{
+		let dtMatch_i=data.my23.dt.filter(myr=>(myr[2]==r[indPos])).filter(myr=>(myr[1]==r[indChr]))
+		if(dtMatch_i.length>0){
+			dtMatch.push(dtMatch_i.concat([r]))
+			//debugger
+		}
+		//debugger
+	})
+	data.pgsMatchMy23=dtMatch
 }
 
 function ui(targetDiv=document.body){ // target div for the user interface
@@ -134,7 +212,7 @@ function ui(targetDiv=document.body){ // target div for the user interface
     div.id='prsCalcUI'
     div.innerHTML=`
     <p>
-    Individual relative risk calculation for 23andme reports based on <a href='https://www.pgscatalog.org' target="_blank">PGS Catalog</a>.
+    Individual risk calculation for 23andme reports based on <a href='https://www.pgscatalog.org' target="_blank">PGS Catalog</a>.
     See also this project's [<a href="https://github.com/episphere/app/tree/main/jonas/prs" target="_blank">code</a>][<a href="https://observablehq.com/@episphere/pgs" target="_blank">notebook</a>][<a href="https://gitter.im/episphere/PRS">discussion</a>].
     </p><p>
 	Below you can select, and inspect, <b style="color:maroon">A)</b> the PGS catalog entry with the risk scores for a list of genomic variations; and <b style="color:maroon">B)</b> Your 23andMe <a href="https://you.23andme.com/tools/data/download" target="_blank">data download</a>. Once you have both (A) and (B), you can proceed to <b style="color:maroon">C)</b> calculate your relative risk for the trait targetted by the PGS entry.
@@ -173,7 +251,9 @@ async function parsePGS(i=4){
     const indInt=[obj.cols.indexOf('chr_position'),obj.cols.indexOf('hm_pos')]
     const indFloat=[obj.cols.indexOf('effect_weight'),obj.cols.indexOf('allelefrequency_effect')]
     const indBol=[obj.cols.indexOf('hm_match_chr'),obj.cols.indexOf('hm_match_pos')]
-    obj.dt=obj.dt.map(r=>{ // for each data row
+    
+	// /* this is the efficient way to do it, but for large files it has memory issues
+	obj.dt=obj.dt.map(r=>{ // for each data row
         indFloat.forEach(ind=>{
             r[ind]=parseFloat(r[ind])
         })
@@ -185,6 +265,7 @@ async function parsePGS(i=4){
         })
         return r
     })
+	// */
     // parse metadata
     obj.meta.txt.filter(r=>(r[1]!='#')).forEach(aa=>{
         aa=aa.slice(1).split('=')
